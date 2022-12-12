8b9d88988ca4ab2b0ea95c78040575b243e3f75e
rloterh <rloterh@gmail.com>
2022-12-12 02:15:34 +0000

    Update fizzbuzz

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -16,10 +16,10 @@ def fizzbuzz(n):
 
     tmp_result = []
     for i in range(1, n + 1):
-        if (i % 3) == 0:
-            tmp_result.append("Fizz")
-        elif (i % 3) == 0 and (i % 5) == 0:
+        if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
         elif (i % 5) == 0:
             tmp_result.append("Buzz")
         else:
@@ -27,7 +27,7 @@ def fizzbuzz(n):
     print(" ".join(tmp_result))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) <= 1:
         print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
